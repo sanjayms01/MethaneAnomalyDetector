711f5135c9c5621d5f6e1b5814d30d00533adf08
Sanjay Saravanan <mssanjay96@gmail.com>
2021-11-14 15:57:14 -0800

    Adding swiper and isotope imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './pages/home';
 import Explore from './pages/explore';
 
+import Swiper from "swiper";
+import Isotope from 'isotope';
+
 class App extends React.Component {
 
     /**
