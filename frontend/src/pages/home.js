03ee52bfa4c100f5903e5f30a2e5a55357c28b3b
Shyamkarthik Rameshbabu <karthik@saavn.com>
2021-11-14 14:39:31 -0800

    import path fix

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,17 +3,17 @@ import { API } from 'aws-amplify'
 import regeneratorRuntime from 'regenerator-runtime'
 
 // STYLES
-import './assets/vendor/animate.css/animate.min.css'
-import './assets/vendor/aos/aos.css'
-import './assets/vendor/bootstrap/css/bootstrap.min.css'
-import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
-import './assets/vendor/boxicons/css/boxicons.min.css'
-import './assets/vendor/glightbox/css/glightbox.min.css'
-import './assets/vendor/remixicon/remixicon.css'
-import './assets/vendor/swiper/swiper-bundle.min.css'
+import '../assets/vendor/animate.css/animate.min.css'
+import '../assets/vendor/aos/aos.css'
+import '../assets/vendor/bootstrap/css/bootstrap.min.css'
+import '../assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import '../assets/vendor/boxicons/css/boxicons.min.css'
+import '../assets/vendor/glightbox/css/glightbox.min.css'
+import '../assets/vendor/remixicon/remixicon.css'
+import '../assets/vendor/swiper/swiper-bundle.min.css'
 
-import './assets/css/style.css';
-import './assets/js/main.js';
+import '../assets/css/style.css';
+import '../assets/js/main.js';
 
 
 export default class Home extends Component {
