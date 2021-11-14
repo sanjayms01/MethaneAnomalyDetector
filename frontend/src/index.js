569372c3f04ac55585b6db3a51e7d355f24fa9b7
Shyamkarthik Rameshbabu <karthik@saavn.com>
2021-11-14 14:14:07 -0800

    css potential fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,5 +4,16 @@ import App from './App';
 import { Amplify } from 'aws-amplify'
 import config from './aws-exports'
 
+// STYLES
+import './assets/css/style.css';
+import './assets/vendor/animate.css/animate.min.css'
+import './assets/vendor/aos/aos.css'
+import './assets/vendor/bootstrap/css/bootstrap.min.css'
+import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import './assets/vendor/boxicons/css/boxicons.min.css'
+import './assets/vendor/glightbox/css/glightbox.min.css'
+import './assets/vendor/remixicon/remixicon.css'
+import './assets/vendor/swiper/swiper-bundle.min.css'
+
 Amplify.configure(config);
 ReactDOM.render(<App />, document.getElementById("root"));
