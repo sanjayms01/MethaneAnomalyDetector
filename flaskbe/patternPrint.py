0bb2984d08fa90d276fb9c0e6ea14a59d7c44383
Shyamkarthik Rameshbabu <karthik@saavn.com>
2021-11-23 16:02:25 -0800

    server print statement

---
+++ flaskbe/patternPrint.py
@@ -0,0 +1,19 @@
+
+def upwardTriangle(n):
+	for i in range(n):
+		for j in range(i,n):
+			print(" ",end=" ")
+
+		for j in range(i+1):
+			print("*", end=" ")
+
+		print()
+
+def downTriangle(n):
+	for i in range(n):
+		for j in range(i + 1):
+			print(" ",end=" ")
+
+		for j in range(i, n):
+			print("*", end=" ")
+		print()
