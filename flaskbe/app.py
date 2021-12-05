10893bf28be2d96a1a97a7a4f9a0a861efc47eff
Shyamkarthik Rameshbabu <karthik@saavn.com>
2021-12-05 11:04:28 -0800

    adding comments to all the routes

--- flaskbe/app.py
+++ flaskbe/app.py
@@ -1,7 +1,6 @@
 import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
 import altair as alt
 import numpy as np
 
@@ -34,10 +33,11 @@ region_poly_list = DL.cl_gdf['geometry'].tolist()
 
 printDiamond("READY TO SERVE")
 
-### TEST ROUTE ###
+####### UTILITY ROUTES #######
 
 @app.route("/")
 def route_homepage():
+    '''Health check that data is loaded and Flask server is up'''
     return jsonify({"zone": get_data_shape(DL.df_zone),
                     "all" : get_data_shape(DL.df_all),
                     "climate_zones" : get_data_shape(DL.cl_gdf),
@@ -47,51 +47,57 @@ def route_homepage():
                     "all_dates_df": get_data_shape(DL.all_dates_df),
                     })
 
-
 @app.route("/get_date_range")
 def route_get_date_range():
+    '''Get start and end date range of data in use'''
     return jsonify({'dates': DL.get_date_range()})
 
 
-### EXPLORE ROUTES ###
+####### EXPLORE ROUTES #######
 
 @app.route("/get_bar_zone_split")
 def route_get_bar_zone_split():
+    '''Section 1: Bar Chart'''
     return jsonify({"chart": get_bar_zone_split(DL.df_all)})
 
 @app.route("/get_feature_dashboard")
 def route_get_feature_dashboard():
+    '''Section 2: Data Comparison Dashboard'''
     time_feature = request.args.get("tfeat")
     bar_feature = request.args.get("bfeat")
     return jsonify({"chart": get_feature_dashboard(DL, time_feature, bar_feature)})
 
-@app.route("/get_vista_ca_dashboard")
-def route_get_vista_ca_dashboard():
-    return jsonify({"chart": get_vista_ca_dashboard(DL)})
-
 @app.route("/get_missing_data_dashboard")
 def route_get_missing_data_dashboard():
+    '''Section 3: Missing Data Analysis Dashboard'''
     resolution = request.args.get("reso")
     resolution = "Zone" if resolution == "Zone" else float(resolution)
-    
     freq = request.args.get("freq")
     return jsonify({"chart": get_missing_data_dashboard(DL, resolution, freq)})
 
 @app.route("/get_missing_data_line")
 def route_get_missing_data_line():
+    '''Section 3: Missing Data Line Chart, by resolution'''
     return jsonify({"chart": CL.missing_data_line_chart.to_json()})
 
+@app.route("/get_vista_ca_dashboard")
+def route_get_vista_ca_dashboard():
+    '''Section 4: Methane Emitters Dashboard'''
+    return jsonify({"chart": get_vista_ca_dashboard(DL)})
 
 
-### PRODUCT ROUTES ###
+####### PRODUCT ROUTES #######
+
 @app.route("/get_location_check")
 def route_get_location_check():
+    '''Check (lat, lng) is in CA'''
     lat = float(request.args.get('lat'))
     lng = float(request.args.get('lng'))
     return jsonify({"result": is_in_california(DL, lat, lng, zone_id_list, region_poly_list)})
 
 @app.route("/get_anomaly_df")
 def route_get_anomaly_df():
+    '''Get anomalies dataframe by climate zone or address locale'''
     args = request.args
 
     if 'zone' in args:
@@ -110,8 +116,8 @@ def route_get_anomaly_df():
 
 @app.route("/get_recent_line_chart")
 def route_get_recent_line_chart():
+    '''Get line chart of methane anomalies last 6 months by zone or address locale'''
     args = request.args
-
     if 'zone' in args:
         z = int(request.args.get('zone'))
         return jsonify({"chart": get_recent_line_chart(DL, z=z)})
@@ -127,8 +133,8 @@ def route_get_recent_line_chart():
 
 @app.route("/get_product_line_chart")
 def route_get_product_line_chart():
+    '''Get long tail line chart of methane anomalies last 3 years by zone or address locale'''
     args = request.args
-
     if 'zone' in args:
         z = int(request.args.get('zone'))
         return jsonify({"chart": get_product_line_chart(DL, z=z)})
@@ -144,8 +150,8 @@ def route_get_product_line_chart():
 
 @app.route("/get_methane_map")
 def route_get_methane_map():
+    '''Get map of CA highlighting either zone or address locale with methane averages in last 6 months'''
     args = request.args
-
     if 'zone' in args:
         z = int(request.args.get('zone'))
         return jsonify({"chart": get_methane_map(DL, z=z)})
@@ -160,6 +166,7 @@ def route_get_methane_map():
 
 @app.route("/get_recent_tweets")
 def route_get_recent_tweets():
+    '''Get real time Twitter feed of methane news in CA'''
     return get_recent_tweets()
 
 #@app.route("/get_address_anomalies")
