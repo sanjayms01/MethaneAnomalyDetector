8a22219ccbf404176d5ec016064b5d6340e585b8
Sanjay Saravanan <mssanjay96@gmail.com>
2021-12-09 11:32:19 -0800

    Updating location path checks

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -7,7 +7,6 @@ export default class Header extends Component {
     }
     render() {
         let activeTab  = window.location.pathname;
-        console.log("ACTIVE TAB", activeTab);
         return (
 
             <header id="header" className="fixed-top d-flex align-items-center ">
@@ -17,8 +16,8 @@ export default class Header extends Component {
                 <nav id="navbar" className="navbar">
                   <ul>
                     <li><a className={(activeTab == '/') ? "nav-link scrollto active" : "nav-link scrollto"} style={{fontSize: 18}} href="/">Home</a></li>
-                    <li><a className={(activeTab == '/model') ? "nav-link scrollto active" : "nav-link scrollto"} style={{fontSize: 18}} href="/model">Model</a></li>
-                    <li className="dropdown"><a className={(activeTab == '/explore' || activeTab == '/product') ? "nav-link scrollto active" : "nav-link scrollto"} style={{fontSize: 18}}><span>Products</span> <i className="bi bi-chevron-down"></i></a>
+                    <li><a className={(activeTab == '/model/') ? "nav-link scrollto active" : "nav-link scrollto"} style={{fontSize: 18}} href="/model">Model</a></li>
+                    <li className="dropdown"><a className={(activeTab == '/explore/' || activeTab == '/product/') ? "nav-link scrollto active" : "nav-link scrollto"} style={{fontSize: 18}}><span>Products</span> <i className="bi bi-chevron-down"></i></a>
                       <ul>
                         <li><a style={{fontSize: 16}} href="/product">Anomaly Detector</a></li>
                         <li><a style={{fontSize: 16}} href="/explore">Data Explorer </a></li>
