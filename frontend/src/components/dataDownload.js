32d45771954cd392a23353ee9cadaf0d400815e1
Sanjay Saravanan <mssanjay96@gmail.com>
2021-12-08 22:47:18 -0800

    Updating links to hashlinks and using full domain

--- frontend/src/components/dataDownload.js
+++ frontend/src/components/dataDownload.js
@@ -39,8 +39,8 @@ export default class DataDownload extends Component {
                         <Tab.Pane eventKey="first">
                             <p id="dwld-box-cz" style={{textAlign: 'left'}}>
                                 This dataset includes all methane and weather readings averaged for each California climate zone for each day from <em>{start_dt} - {end_dt}</em>. 
-                                To better understand what climate zones are and to locate your zone, refer to {(currentPage == 'Model') ? <a href="./explore#data_explorer_intro" target="_blank">Zone Details</a> : <Link to="/explore#data_explorer_intro">Zone Details</Link>}.
-                                To better understand each of the time series columns, refer to the glossary in {(currentPage == 'Model') ? <a href="./explore#data_comparison" target="_blank">Data Comparison</a> : <HashLink to="/explore#data_comparison">Data Comparison</HashLink>}.
+                                To better understand what climate zones are and to locate your zone, refer to {(currentPage == 'Model') ? <a href="https://methaneanomalydetector.com/explore#data_explorer_intro" target="_blank">Zone Details</a> : <HashLink to="/explore#data_explorer_intro">Zone Details</HashLink>}.
+                                To better understand each of the time series columns, refer to the glossary in {(currentPage == 'Model') ? <a href="https://methaneanomalydetector.com/explore#data_comparison" target="_blank">Data Comparison</a> : <HashLink to="/explore#data_comparison">Data Comparison</HashLink>}.
                                 <br/>
                                 <br/>
                                 <a href="https://methane-capstone.s3.us-west-2.amazonaws.com/public_data/zone-data.csv" className="btn-get-started">Download</a>
@@ -49,8 +49,8 @@ export default class DataDownload extends Component {
                         <Tab.Pane eventKey="second">
                             <p id="dwld-box-full" style={{textAlign: 'left'}}>
                                 This cleaned dataset includes methane and weather readings for each day from <em>{start_dt} - {end_dt}</em>.
-                                To better understand the rounded resolution columns, refer to {(currentPage == 'Model') ? <a href="./explore#missing_data" target="_blank">Missing Data</a> : <Link to="/explore#missing_data">Missing Data</Link>}.
-                                To better understand each of the time series columns, refer to the glossary in {(currentPage == 'Model') ? <a href="./explore#data_comparison" target="_blank">Data Comparison</a> : <HashLink to="/explore#data_comparison">Data Comparison</HashLink>}.
+                                To better understand the rounded resolution columns, refer to {(currentPage == 'Model') ? <a href="https://methaneanomalydetector.com/explore#missing_data" target="_blank">Missing Data</a> : <HashLink to="/explore#missing_data">Missing Data</HashLink>}.
+                                To better understand each of the time series columns, refer to the glossary in {(currentPage == 'Model') ? <a href="https://methaneanomalydetector.com/explore#data_comparison" target="_blank">Data Comparison</a> : <HashLink to="/explore#data_comparison">Data Comparison</HashLink>}.
                                 <br/>
                                 <br/>
                                 <a href='https://methane-capstone.s3.us-west-2.amazonaws.com/public_data/full-raw-data.csv' className="btn-get-started">Download</a>
